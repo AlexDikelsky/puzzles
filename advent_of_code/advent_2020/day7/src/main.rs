33f3d9be4fcdc5c0a99a5267f842180a58d98441
Alex Dikelsky <alexander.dikelsky@gmail.com>
2020-12-08 08:37:23 -0600

    Finished part 2

--- advent_of_code/advent_2020/day7/src/main.rs
+++ advent_of_code/advent_2020/day7/src/main.rs
@@ -4,37 +4,41 @@ use std::collections::HashSet;
 use std::collections::HashMap;
 
 fn main() {
-    //let file_in = fs::read_to_string("c.txt").unwrap();
+    // let file_in = fs::read_to_string("b.txt").unwrap();
     let file_in = fs::read_to_string("day7_in.txt").unwrap();
 
-    let number_name = Regex::new(r"(\d+) ([A-z]+ [A-z]+) bags?").unwrap();
+    let number_name = Regex::new(r"(\d+) ([A-z]+ [A-z]+) bags?.?").unwrap();
     let name_only = Regex::new("([A-z]+ [A-z]+) bags?").unwrap();
 
-    for _ in 0..1 {
-        for line in file_in.lines() {
-            let s: Vec<&str> = line.split(" contain ").collect();
-            assert!(s.len() == 2);
-            let container = name_only.captures_iter(s[0]).next().unwrap()[1].to_string();
-            let contents: Vec<&str> = s[1].strip_suffix(".").unwrap().split(", ").collect();
-
-            for bag_name_with_extra in contents.iter() {
-                // dbg!(&bag_name_with_extra);
-                match bag_name_with_extra == &"no other bags" {
-                    true  => (),
-                    false => {
-                        let bag_name = 
-                            &number_name.captures_iter(bag_name_with_extra).next().unwrap()[2];
-                        // dbg!(&bag_name);
-                        if contain_golden.contains(bag_name) {
-                            // dbg!(&bag_name);
-                            contain_golden.insert(container.clone());
-                        };
-                    }
-                };
+    let inputs: HashMap<String, Vec<(String, usize)>> = file_in
+        .lines()
+        .map(|line| {
+            let split: Vec<&str> = line.split(" contain ").collect();
+            let container_name = name_only.captures(split[0]).unwrap()[1].to_string();
+            let contents_names = if split[1] != "no other bags." {
+                split[1]
+                    .split(", ")
+                    .map(|group| {
+                        let matched_name = number_name.captures(group).unwrap();
+                        (
+                            matched_name[2].to_string(),
+                            matched_name[1].parse().unwrap(),
+                        )
+                    })
+                    .collect()
+            } else {
+                vec![]
             };
-        };
-        before_len = after_len;
-        after_len = contain_golden.len();
-    }
-    dbg!(&contain_golden, after_len - 1);
+            (container_name, contents_names)
+        })
+        .collect();
+
+    // dbg!(&inputs);
+    dbg!(part2("shiny gold", &inputs) - 1);
+}
+
+fn part2(search_for: &str, map: &HashMap<String, Vec<(String, usize)>>) -> usize {
+    map[search_for].iter().fold(1, |acc, bag| {
+        acc + bag.1 * part2(&bag.0, map)
+    })
 }
