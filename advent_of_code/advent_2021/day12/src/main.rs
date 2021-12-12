bebe9c9a084370fc40334eb2493f8f7defb5f79c
Alex Dikelsky <alexander.dikelsky@gmail.com>
2021-12-12 01:27:50 -0600

    Fixing something

--- advent_of_code/advent_2021/day12/src/main.rs
+++ advent_of_code/advent_2021/day12/src/main.rs
@@ -1,17 +1,26 @@
+use crate::VisitsLeft::{One, Zero};
 use itertools::Itertools;
 use std::collections::BTreeMap;
 use std::collections::BTreeSet;
 use std::fs;
 use std::iter;
-use crate::VisitsLeft::{Zero, One, Two};
 
 type Graph<'a> = BTreeMap<&'a str, BTreeSet<&'a str>>;
 
-#[derive(Debug,Copy,Clone,Eq,PartialEq)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq, PartialOrd, Ord)]
 enum VisitsLeft {
     Zero,
     One,
-    Two,
+}
+
+impl VisitsLeft {
+    pub fn dec(&self) -> Self {
+        match self {
+            Zero => panic!("Dec of zero"),
+            One => Zero,
+            Two => One,
+        }
+    }
 }
 
 fn main() {
@@ -33,14 +42,38 @@ fn main() {
 
     let graph: Graph = v.iter().fold(graph, |g, (a, b)| connect(a, b, g));
 
+    let init = vec![("start", Zero), ("end", One)];
+
     // dbg!(&graph);
     // dbg!(follow(&graph, "start", "end", BTreeSet::from_iter(iter::once("start"))));
-    dbg!(follow(&graph, "start", "end", BTreeSet::from_iter(iter::once("start"))).len());
+    dbg!(yikes(follow(
+        &graph,
+        "start",
+        "end",
+        BTreeMap::from_iter(init.clone().into_iter())
+    )));
+
+    dbg!(yikes(follow(
+        &graph,
+        "start",
+        "end",
+        BTreeMap::from_iter(init.into_iter())
+    )).len());
 }
 
 fn is_small(v: &str) -> bool {
     v.to_lowercase() == v
 }
+
+// fn yikes(v: BTreeSet<Vec<&str>>) -> BTreeSet<Vec<&str>> {
+//     v.into_iter().filter(|path| {
+//         let mut path = path.into_iter().filter(|name| is_small(name)).collect_vec();
+//         path.sort_unstable();
+//         let v = path.len();
+//         path.dedup();
+//         (v == path.len()) || (path.len() + 1 == v)
+//     }).collect()
+// }
 
 fn connect<'a>(a: &'a str, b: &'a str, g: Graph<'a>) -> Graph<'a> {
     g.into_iter()
@@ -56,29 +89,43 @@ fn follow<'a>(
     g: &'a Graph,
     start: &'a str,
     end: &'a str,
-    visited: BTreeSet<&'a str>,
+    visited: BTreeMap<&'a str, VisitsLeft>,
 ) -> BTreeSet<Vec<&'a str>> {
     if start == end {
         BTreeSet::from_iter(iter::once(vec![end]))
     } else {
         g[start]
             .iter()
-            .filter(|n| !visited.contains(*n))
-            .map(|n| {
-                follow(
-                    g,
-                    n,
-                    end,
-                    if is_small(n) {
-                        visited.clone().into_iter().chain(iter::once(*n)).collect()
-                    } else {
-                        visited.clone()
-                    },
-                )
-                .into_iter()
-                .map(|path| iter::once(start).chain(path.into_iter()).collect())
-            })
-            .flatten()
+            .filter_map(|n| {
+                let visited = if is_small(n) {
+                    match visited.get(*n) {
+                        Some(Zero) => None,
+                        Some(One) => Some(
+                            visited
+                                .clone()
+                                .into_iter()
+                                .chain(iter::once((*n, Zero)))
+                                .collect(),
+                        ),
+                        None => Some(
+                            visited
+                                .clone()
+                                .into_iter()
+                                .chain(iter::once((*n, One)))
+                                .collect(),
+                        ),
+                    }
+                } else {
+                    Some(visited.clone())
+                };
+                visited.map(|vis| {
+                    follow(g, n, end, vis).into_iter().map(|path: Vec<&str>| {
+                        iter::once(start)
+                            .chain(path.into_iter())
+                            .collect::<Vec<&str>>()
+                    })
+                })
+            }).flatten()
             .collect()
     }
 }
