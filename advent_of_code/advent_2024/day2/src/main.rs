662d0f05ad94cfcc908e1afb01fd0bd093b1ec07
Alex Dikelsky <alexander.dikelsky@gmail.com>
2024-12-06 17:44:17 -0800

    working

--- advent_of_code/advent_2024/day2/src/main.rs
+++ advent_of_code/advent_2024/day2/src/main.rs
@@ -7,13 +7,17 @@ fn main() {
     // dbg!(&schedule);
     // dbg!(parsed.iter().map(m).collect::<Vec<Vec<isize>>>());
     let k = parsed.iter().map(m).map(check).collect::<Vec<bool>>();
-    let l = parsed.iter().map(m).map(check2).collect::<Vec<bool>>();
+    let l = parsed.iter().map(m2).map(check2).collect::<Vec<bool>>();
     dbg!(k.into_iter().map(|x| if x {1} else {0}).sum::<isize>());
-    dbg!(l.into_iter().map(|x| if x {1} else {0}).sum::<isize>());
-}
+    dbg!(l.into_iter().map(|x| if x {1} else {0}).sum::<isize>()); }
 
 fn m(v: &Vec<isize>) -> Vec<isize> {
   v.iter().zip(v.iter().skip(1)).map(|(a, b)| a - b).collect()
+}
+
+fn m2(v: &Vec<isize>) -> Vec<(isize, isize)> {
+  v.iter().zip(v.iter().skip(1)).zip(v.iter().skip(2))
+    .map(|((a, b), c)| (a - b, a - c)).collect()
 }
 
 fn check(v: Vec<isize>) -> bool {
@@ -21,9 +25,9 @@ fn check(v: Vec<isize>) -> bool {
   v.iter().all(|x| (*x >= -3) && (*x < 0))
 }
 
-fn check2(v: Vec<isize>) -> bool {
-  let g = v.iter().filter(|x| (**x <= 3) && (**x > 0)).collect::<Vec<&isize>>().len() <= 1;
-  let h = v.iter().filter(|x| (**x >= -3) && (**x < 0)).collect::<Vec<&isize>>().len() <= 1;
-  println!("{}, {}, {:?}", g, h, &v);
-  g || h
+fn check2(v: Vec<(isize, isize)>) -> bool {
+  let h:Vec<&(isize, isize)> = v.iter().zip(0..).find(|((x,y), _)| (*x <= 3) && (*x > 0) && (*y <= 6) && (*y > 1)).collect();
+  let i:Vec<&(isize, isize)> = v.iter().zip(0..).find(|((x,y), _)| (*x >= -3) && (*x < 0) && (*y >= -6) && (*y < -1)).collect();
+  dbg!(&h, &i, h.len() < 1 || i.len() < 1);
+  false
 }
