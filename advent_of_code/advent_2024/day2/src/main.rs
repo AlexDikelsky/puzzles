74e37058220de400b85b795166e96a19ebdafacb
Alex Dikelsky <alexander.dikelsky@gmail.com>
2024-12-06 19:02:31 -0800

    3

--- advent_of_code/advent_2024/day2/src/main.rs
+++ advent_of_code/advent_2024/day2/src/main.rs
@@ -6,9 +6,9 @@ fn main() {
 
     // dbg!(&schedule);
     // dbg!(parsed.iter().map(m).collect::<Vec<Vec<isize>>>());
-    let k = parsed.iter().map(m).map(check).collect::<Vec<bool>>();
-    let l = parsed.iter().map(m2).map(check2).collect::<Vec<bool>>();
-    dbg!(k.into_iter().map(|x| if x {1} else {0}).sum::<isize>());
+    // let k = parsed.iter().map(m).map(check).collect::<Vec<bool>>();
+    let l = parsed.iter().map(|line| check2(&m(line), line)).collect::<Vec<bool>>();
+    // dbg!(k.into_iter().map(|x| if x {1} else {0}).sum::<isize>());
     dbg!(l.into_iter().map(|x| if x {1} else {0}).sum::<isize>()); }
 
 fn m(v: &Vec<isize>) -> Vec<isize> {
@@ -21,13 +21,30 @@ fn m2(v: &Vec<isize>) -> Vec<(isize, isize)> {
 }
 
 fn check(v: Vec<isize>) -> bool {
+  println!("checking {:?}", &v);
   v.iter().all(|x| (*x <= 3) && (*x > 0)) ||
   v.iter().all(|x| (*x >= -3) && (*x < 0))
 }
 
-fn check2(v: Vec<(isize, isize)>) -> bool {
-  let h:Vec<&(isize, isize)> = v.iter().zip(0..).find(|((x,y), _)| (*x <= 3) && (*x > 0) && (*y <= 6) && (*y > 1)).collect();
-  let i:Vec<&(isize, isize)> = v.iter().zip(0..).find(|((x,y), _)| (*x >= -3) && (*x < 0) && (*y >= -6) && (*y < -1)).collect();
-  dbg!(&h, &i, h.len() < 1 || i.len() < 1);
-  false
+fn check2(v: &Vec<isize>, p: &Vec<isize>) -> bool {
+  let asc = v.iter().zip(0..).find(|(x, _)| (**x <= 3) && (**x > 0));
+  let dsc = v.iter().zip(0..).find(|(x, _)| (**x >= -3) && (**x < 0));
+  println!("{:?}, {:?}: {:?}", asc, dsc, v);
+  match (asc, dsc) {
+   (None, _) => check(v.to_vec()),
+   (_, None) => check(v.to_vec()),
+   (Some((_, index)), _) => check(p[..index].iter().chain(p[(index+2)..].iter()).copied().collect()),
+   (_, Some((_, index))) => check(p[..index].iter().chain(p[(index+2)..].iter()).copied().collect()),
+  }
+}
+
+// fn check2(v: Vec<(isize, isize)>) -> bool {
+//   let h:Vec<&(isize, isize)> = v.iter().zip(0..).find(|((x,y), _)| (*x <= 3) && (*x > 0) && (*y <= 6) && (*y > 1)).collect();
+//   let i:Vec<&(isize, isize)> = v.iter().zip(0..).find(|((x,y), _)| (*x >= -3) && (*x < 0) && (*y >= -6) && (*y < -1)).collect();
+//   dbg!(&h, &i, h.len() < 1 || i.len() < 1);
+//   false
+// }
+
+
+fn c3(v: &Vec<isize) -> bool {
 }
