3daa032f2ddecc151d19c057ef2eede5df25ef3c
Alex Dikelsky <alexander.dikelsky@gmail.com>
2024-12-07 16:54:43 -0800

    Part 1

--- advent_of_code/advent_2024/day6/src/main.rs
+++ advent_of_code/advent_2024/day6/src/main.rs
@@ -2,8 +2,8 @@ use crate::Heading::*;
 use crate::Terrain::*;
 use itertools::FoldWhile::{Continue, Done};
 use itertools::Itertools;
-use std::fs;
 use std::collections::HashSet;
+use std::fs;
 use std::iter;
 
 #[derive(Debug, Copy, Clone)]
@@ -17,7 +17,7 @@ enum Heading {
 #[derive(Debug, Copy, Clone)]
 enum Terrain {
     Empty(usize, usize),
-    Wall(usize, usize),
+    Wall,
 }
 
 type Po = (usize, usize);
@@ -52,7 +52,7 @@ impl D {
 }
 
 fn main() {
-    let file_in = fs::read_to_string("data.txt").unwrap();
+    let file_in = fs::read_to_string("data2.txt").unwrap();
     let mut person = (0, 0);
     let m: Vec<Vec<Terrain>> = file_in
         .lines()
@@ -62,7 +62,7 @@ fn main() {
                 .zip(0..)
                 .map(|(c, col)| match c {
                     '.' => Empty(row, col),
-                    '#' => Wall(row, col),
+                    '#' => Wall,
                     '^' => {
                         person = (row, col);
                         Empty(row, col)
@@ -76,21 +76,25 @@ fn main() {
 
     dbg!(surr(&m, (N, (0, 0))));
 
-    let z = (0..).fold_while((person, HashSet::new()), |(acc, set), _| {
-       // dbg!(&acc);
-       match next(&m, acc) {
-          None => Done((acc, set)),
-          Some((_, None)) => Done((acc, set)),
-          Some((_, Some(Wall(_, _)))) => panic!("Ran through wall"),
-          Some((heading, Some(Empty(ny, nx)))) => Continue(((heading, (ny, nx)), a(set, (ny, nx)))),
-       }
-    }).into_inner();
-    
+    let z = (0..)
+        .fold_while(
+            (person, vec![person.1].into_iter().collect()),
+            |(acc, set), _| match next(&m, acc) {
+                None => Done((acc, set)),
+                Some((_, None)) => Done((acc, set)),
+                Some((_, Some(Wall))) => panic!("Ran through wall"),
+                Some((heading, Some(Empty(ny, nx)))) => {
+                    Continue(((heading, (ny, nx)), a(set, (ny, nx))))
+                }
+            },
+        )
+        .into_inner();
+
     dbg!(z.1.len());
 }
 
 fn a(s: HashSet<Po>, v: Po) -> HashSet<Po> {
-  s.into_iter().chain(iter::once(v)).collect()
+    s.into_iter().chain(iter::once(v)).collect()
 }
 
 fn surr(m: &M, p: P) -> D {
@@ -98,23 +102,21 @@ fn surr(m: &M, p: P) -> D {
     D {
         north: y
             .checked_sub(1)
-            .map(|nn| m.get(nn).map(|a| a.get(x)))
-            .flatten()
+            .and_then(|nn| m.get(nn).map(|a| a.get(x)))
             .flatten()
             .copied(),
-        south: m.get(y + 1).map(|a| a.get(x)).flatten().copied(),
-        east: m.get(y).map(|a| a.get(x + 1)).flatten().copied(),
+        south: m.get(y + 1).and_then(|a| a.get(x)).copied(),
+        east: m.get(y).and_then(|a| a.get(x + 1)).copied(),
         west: x
             .checked_sub(1)
-            .map(|nn| m.get(y).map(|a| a.get(nn)))
-            .flatten()
+            .and_then(|nn| m.get(y).map(|a| a.get(nn)))
             .flatten()
             .copied(),
     }
 }
 
 fn next(m: &M, p: P) -> Option<(Heading, Option<Terrain>)> {
-    let (h, po) = p;
+    let (h, _) = p;
     let s = surr(m, p);
     let next_location = match h {
         N => s.north,
@@ -124,6 +126,6 @@ fn next(m: &M, p: P) -> Option<(Heading, Option<Terrain>)> {
     };
     next_location.map(|terr| match terr {
         Empty(_, _) => s.forward(h),
-        Wall(_, _) => s.right(h),
+        Wall => s.right(h),
     })
 }
